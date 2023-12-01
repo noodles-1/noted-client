4dbccbef69159385dc5bb4c31352fbc4f2d4f89a
noodelz <adrianeroa081202@gmail.com>
2023-12-01 10:32:14 +0800

    initialized basic home and notes view

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div className="blur-effect blur-dark-bg h-full w-[240px] text-gray-100 flex items-center justify-center rounded-tl-[12px] rounded-bl-[12px]">
+        <h1> Home components </h1>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { Home, Notes } from "@/app/components";
+
+export default function Main() {
   return (
     <main>
-
+        <div className="wrapper">
+            <Home />
+            <Notes />
+        </div>
     </main>
   )
 }
