7a810d713bd4794abb3a80662cf16a496d2ea19e
noodelz <adrianeroa081202@gmail.com>
2023-12-15 01:28:43 +0800

    fixed multiple connections on redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,8 +1,29 @@
 import { createClient } from "redis";
+import { RedisConnection } from "redis-om";
 
-export const client = createClient({
-    url: process.env.REDIS_URL
-})
+declare global {
+    var redisClient: RedisConnection
+}
 
-if (!client.isOpen) 
-    client.connect()
+export let client: RedisConnection
+
+if (process.env.NODE_ENV === 'development') {
+    if (!global.redisClient) {
+        global.redisClient = createClient({
+            url: process.env.REDIS_URL
+        })
+        if (!global.redisClient.isOpen)
+            global.redisClient.connect()
+        client = global.redisClient
+    }
+
+    client = global.redisClient
+}
+else {
+    client = createClient({
+        url: process.env.REDIS_URL
+    })
+
+    if (!client.isOpen)
+        client.connect()
+}
